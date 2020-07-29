2f6bd9da09c1bf7f357da9b51f336844688bef63
Brooke Gibbons <brooke.gibbons@uwa.edu.au>
2020-07-29 14:42:05 +0800

    new files and upgrades

--- global.R
+++ global.R
@@ -1,54 +1,64 @@
-library(shinydashboard)
-library(leaflet)
-library(shiny)
-library(shinythemes)
-library(shinyBS)
-library(shinyjs)
-library(DT)
-library(profvis)
+# Load libraries ----
 library(dplyr)
-library(mapview)
-library(leafpop)
-library(sf)
-# library(gdal)
-library(htmlwidgets)
+library(DT)
+library(forcats)
+library(ggplot2)
 library(GlobalArchive)
-
+library(htmlwidgets)
+library(leaflet)
+library(leafpop)
+library(mapview)
+library(profvis)
+library(raster)
 library(rgdal)
 library(rgeos)
-library(raster)
 library(rmapshaper)
-library(ggplot2)
+library(shiny)
+library(shinyBS)
+library(shinydashboard)
+library(shinythemes)
+library(shinyjs)
+library(sf)
 library(stringr)
-library(forcats)
 
+# Bring in data ----
 # Load 2019 ningaloo metadata ----
-ning.bruv.metadata<-read.csv("data/2019-08_Ningaloo_metadata.csv")%>%
-  mutate(sample=as.character(sample))
+ning.bruv.metadata <- read.csv("data/2019-08_Ningaloo_metadata.csv") %>%
+  mutate(sample = as.character(sample))
 
-# Load 2014 Geographe bay metadata ----
-gb.bruv.metadata<-read.csv("data/2014-12_Geographe.Bay_stereoBRUVs_Metadata.csv")
+# Load 2014 geographe bay metadata ----
+gb.bruv.metadata <- read.csv("data/2014-12_Geographe.Bay_stereoBRUVs_Metadata.csv")
+
+# Load 2020 south west metadata ----
+sw.bruv.metadata <- read.csv("data/2020-06_south-west_stereoBRUVs_metadata.csv")
 
 # Create dataframe for 2014 Geographe Bay BRUV images for plotting ----
-gb.bruv.image<-gb.bruv.metadata%>%
-  dplyr::mutate(image=paste0("https://marineecology.io/images/2014-12_BRUVs_Forward/",Sample,".jpg",sep=""))%>%
-  ga.clean.names()%>%
-  dplyr::mutate(source="stereo-bruv.image")%>%
-  mutate(height='"230"')%>%mutate(width='"405"')%>%
-  mutate(image=paste0('<iframe src=',image,' height=',height,' width=',width,'></iframe>'))%>%
+gb.bruv.image <- gb.bruv.metadata %>%
+  dplyr::mutate(image=paste0("https://marineecology.io/images/2014-12_BRUVs_Forward/",Sample,".jpg",sep="")) %>%
+  ga.clean.names() %>%
+  dplyr::mutate(source="stereo-bruv.image") %>%
+  mutate(height='"230"') %>% mutate(width='"405"') %>%
+  mutate(image=paste0('<iframe src=',image,' height=',height,' width=',width,'></iframe>')) %>%
   glimpse()
 
 # Create dataframe for 2019 Ningaloo BRUV images for plotting ----
-ning.bruv.image<-ning.bruv.metadata%>%
-  dplyr::mutate(image=paste0("https://marineecology.io/images/habitatmapp/ningaloo/",sample,".jpg",sep=""))%>% # NEED TO UPDATE THIS
-  ga.clean.names()%>%
-  dplyr::mutate(source="stereo-bruv.image")%>%
+ning.bruv.image <- ning.bruv.metadata %>%
+  dplyr::mutate(image=paste0("https://marineecology.io/images/habitatmapp/ningaloo/",sample,".jpg",sep="")) %>% 
+  ga.clean.names() %>%
+  dplyr::mutate(source="stereo-bruv.image") %>%
   mutate(height='"365"')%>%mutate(width='"645"')%>%
-  mutate(image=paste0('<iframe src=',image,' height=',height,' width=',width,'></iframe>'))%>%
+  mutate(image=paste0('<iframe src=',image,' height=',height,' width=',width,'></iframe>')) %>%
   glimpse()
 
-
-# https://raw.githubusercontent.com/BrookeGibbons/habitatMAPP/master/images/3.01.jpg
+# Create dataframe for 2019 Ningaloo BRUV images for plotting ----
+sw.bruv.image <- sw.bruv.metadata %>%
+  ga.clean.names() %>%
+  dplyr::mutate(image=paste0("https://marineecology.io/images/habitatmapp/sw/",sample,".jpg",sep="")) %>% # NEED TO UPDATE THIS
+  ga.clean.names() %>%
+  dplyr::mutate(source="stereo-bruv.image") %>%
+  mutate(height='"365"')%>%mutate(width='"645"') %>%
+  mutate(image=paste0('<iframe src=',image,' height=',height,' width=',width,'></iframe>')) %>%
+  glimpse()
 
 # Tempory video links ----
 gb.bruv.video<-data.frame(c(-33.6249992,-33.6190304,-33.42207),
@@ -92,27 +102,135 @@ gb.auv.video<-gb.auv.video%>%
   dplyr::mutate(source="auv.video")
 
 # Merge data together for leaflet map ----
-gb.dat<-bind_rows(gb.bruv.image,gb.bruv.video,gb.auv.video)%>%
-  dplyr::select(latitude,longitude,image,bruv.video,auv.video,source)%>%
-  dplyr::mutate(marine.park="Geographe Bay")%>%
+gb.dat <- bind_rows(gb.bruv.image,gb.bruv.video,gb.auv.video) %>%
+  dplyr::select(latitude,longitude,image,bruv.video,auv.video,source) %>%
+  dplyr::mutate(marine.park="Geographe Bay") %>%
   glimpse()
 
-ning.dat<-bind_rows(ning.bruv.image)%>%
-  dplyr::select(latitude,longitude,image,source)%>% # ,bruv.video,auv.video,source
-  dplyr::mutate(marine.park="Ningaloo")%>%
+ning.dat <- bind_rows(ning.bruv.image) %>%
+  dplyr::select(latitude,longitude,image,source) %>% # ,bruv.video,auv.video,source
+  dplyr::mutate(marine.park="Ningaloo") %>%
   glimpse()
 
-sw.dat<-bind_rows(ning.bruv.image)%>%
-  dplyr::select(latitude,longitude,image,source)%>% # ,bruv.video,auv.video,source
-  dplyr::mutate(marine.park="South-west Corner")%>%
+sw.dat <- bind_rows(sw.bruv.image) %>%
+  dplyr::select(latitude,longitude,image,source) %>% # ,bruv.video,auv.video,source
+  dplyr::mutate(marine.park="South-west Corner") %>%
   glimpse()
 
-dat<-bind_rows(gb.dat,ning.dat, sw.dat)
+dat <- bind_rows(gb.dat,ning.dat, sw.dat)
 
 # Make icon for images and videos----
 icon.image <- makeAwesomeIcon(icon = "image", library = "fa")
 icon.video <- makeAwesomeIcon(icon = "video-camera", library = "fa", markerColor = "lightred", iconColor = "black")
 icon.laptop <- makeAwesomeIcon(icon = "laptop", library = "fa", markerColor = "orange", iconColor = "black")
+
+IconSet <- awesomeIconList(
+  "stereo-BRUV video"   = makeAwesomeIcon(icon = "video-camera", library = "fa", markerColor = "lightred"),
+  "stereo-BRUV image" = makeAwesomeIcon(icon = "image", library = "fa"),
+  "AUV 3D models" = makeAwesomeIcon(icon = "laptop", library = "fa", markerColor = "orange")
+)
+
+# Spatial files ----
+# State marine parks ----
+ngari.mp <- readOGR("data/spatial/test1.shp") 
+state.mp <- readOGR("data/spatial/WA_MPA_2018.shp")
+
+# filter out unassigned and unclassified
+state.mp <- state.mp[!state.mp$ZONE_TYPE %in% c("Unassigned (IUCN IA)","Unassigned (IUCN II)","Unassigned (IUCN III)","Unassigned (IUCN IV)","Unassigned (IUCN VI)","MMA (Unclassified) (IUCN VI)","MP (Unclassified) (IUCN VI)"), ]
+
+# remove all alphanumeric to rename zone type
+state.mp$zone<-str_replace_all(state.mp$ZONE_TYPE, c("[^[:alnum:]]"=" "))
+state.mp$zone<-str_replace_all(state.mp$zone, c("Conservation Area  IUCN IA "="Conservation",
+                                                          "General Use  IUCN II "="General Use",
+                                                          "General Use Area  IUCN VI "="General Use",
+                                                          "General Use Zone  IUCN II "="General Use",
+                                                          "Recreation Area  IUCN II "="Recreation",
+                                                          "Recreation Zone  IUCN II "="Recreation",
+                                                          "Sanctuary Area  IUCN VI "="Sanctuary",
+                                                          "Sanctuary Zone  IUCN IA "="Sanctuary",
+                                                          "Special Purpose Zone  Aquaculture   IUCN VI " ="Special Purpose",
+                                                          "Special Purpose Zone  Benthic Protection   IUCN IV "="Special Purpose",
+                                                          "Special Purpose Zone  Dugong Protection   IUCN IV "="Special Purpose", 
+                                                          "Special Purpose Zone  Habitat Protection   IUCN IV " ="Special Purpose",
+                                                          "Special Purpose Zone  Pearling   IUCN VI "  ="Special Purpose",    
+                                                          "Special Purpose Zone  Puerulus   IUCN IA "  ="Special Purpose", 
+                                                          "Special Purpose Zone  Scientific Reference   IUCN II "="Special Purpose",
+                                                          "Special Purpose Zone  Scientific Reference   IUCN VI "="Special Purpose",
+                                                          "Special Purpose Zone  Seagrass Protection   IUCN IV "="Special Purpose", 
+                                                          "Special Purpose Zone  Shore Based Activities   IUCN II "="Special Purpose",
+                                                          "Special Purpose Zone  Wildlife Conservation   IUCN VI "="Special Purpose",
+                                                          "Special Purpose Zone  Wildlife Viewing and Protection   IUCN IV "="Special Purpose",
+                                                          "Special Purpose Zone 1  Shore based Activities   IUCN II "="Special Purpose",       
+                                                          "Special Purpose Zone 2  Shore based Activities   IUCN II "="Special Purpose",       
+                                                          "Special Purpose Zone 3  Shore based Activities   IUCN II " ="Special Purpose",      
+                                                          "Special Purpose Zone 3  Shore based Activities   IUCN VI " ="Special Purpose",      
+                                                          "Special Purpose Zone 4  Shore based Activities   IUCN II "="Special Purpose"))
+
+# Commonwealth marine parks ----
+commonwealth.mp <- readOGR("data/spatial/AustraliaNetworkMarineParks.shp")
+commonwealth.mp$zone<-str_replace_all(commonwealth.mp$ZoneName, c("[^[:alnum:]]"=" "))
+commonwealth.mp$zone<-str_replace_all(commonwealth.mp$zone, c(" Zone"="",
+                                                                      "Habitat Protection  Lord Howe "="Habitat Protection",
+                                                                      "Habitat Protection  Reefs "="Habitat Protection",
+                                                                      "Marine National Park"="National Park",
+                                                                      "Special Purpose  Mining Exclusion "="Special Purpose",
+                                                                      "Special Purpose  Norfolk "="Special Purpose",
+                                                                      "Special Purpose  Trawl "="Special Purpose"))
+
+# Create factors for legends and plotting ----
+# State marine parks ----
+state.mp$zone <- as.factor(state.mp$zone)
+state.mp$zone<-fct_relevel(state.mp$zone, "Conservation", "Sanctuary", "Special Purpose", "Recreation", "General Use")
+
+state.pal <- colorFactor(c("#bfaf02", # conservation
+                           "#c1d72f", # sanctuary
+                           "#ccc1d6", # special purpose
+                           "#f4eb0a", # recreational
+                           '#b5e2ed' # general use
+), state.mp$zone)
+
+# Commonwealth marine parks ----
+commonwealth.mp$zone <- as.factor(commonwealth.mp$zone)
+commonwealth.mp$zone<-fct_relevel(commonwealth.mp$zone, "Sanctuary", "National Park", "Recreational Use", "Habitat Protection", "Multiple Use", "Special Purpose")
+
+commonwealth.pal <- colorFactor(c("#f6c1d9", # Sanctuary
+                         "#7bbc63", # National Park
+                         "#fdb930", # Recreational Use
+                         "#fff7a3", # Habitat Protection
+                         '#b9e6fb', # Multiple Use
+                         '#6daee0'# Special Purpose
+), commonwealth.mp$zone)
+
+# To plot using IUCN ----
+# commonwealth.mp$ZoneIUCN <- str_replace_all(commonwealth.mp$ZoneIUCN,c("IA"="Ia"))
+
+# commonwealth.mp$ZoneIUCN <- fct_collapse(commonwealth.mp$ZoneIUCN,
+#                                          Ia = c("Ia", "IA"),
+#                                          II = "II",
+#                                          IV = "IV",
+#                                          VI = "VI")
+# commonwealth.mp$IUCN <- as.factor(commonwealth.mp$ZoneIUCN)
+# unique(commonwealth.mp$IUCN)
+# factpal <- colorFactor(c("#f6c1d9", "#7bbc63",
+#                          "#fff7a3","#6daee0"), commonwealth.mp$IUCN) # use when only displaying IUCN
+# testpal<-colorFactor(c("#f6c1d9", "#7bbc63",
+#                        "#fff7a3","#6daee0"),
+#                      levels=c("Ia", "II", "IV", "VI"))
+
+## Colours for plotting ----
+# Commonwealth colours
+#f6c1d9 - pink 1A, 1a
+#7bbc63 - green IUCN II
+#fff7a3 - yellow IUCN IV
+#b9e6fb - light blue VI
+#6daee0 - dark blue VI
+
+# State colours
+#bfaf02 - conservation
+#c1d72f - sanctuary
+#ccc1d6 - special purpose
+#f4eb0a - recreational 
+#b5e2ed - general use
 
 # Legend function ----
 markerLegendHTML <- function(IconSet) {
@@ -134,45 +252,3 @@ markerLegendHTML <- function(IconSet) {
   }
   paste0(legendHtml, "</div>")
 }
-
-IconSet <- awesomeIconList(
-  "stereo-BRUV video"   = makeAwesomeIcon(icon = "video-camera", library = "fa", markerColor = "lightred"),
-  "stereo-BRUV image" = makeAwesomeIcon(icon = "image", library = "fa"),
-  "AUV 3D models" = makeAwesomeIcon(icon = "laptop", library = "fa", markerColor = "orange")
-)
-
-
-# Spatial files ----
-
-# state.mp <- readOGR("data/spatial/test1.shp")
-# state.mp <- state.mp[state.mp$Name %in% c("East Geographe Bay Sanctuary Zone", "Busselton Jetty Sanctuary Zone","Central Geographe Bay Sanctuary Zone","Eagle Bay Sanctuary Zone","Cape Naturaliste Sanctuary Zone","Injidup Sanctuary Zone","Wyadup Sanctuary Zone","Yallingup Sanctuary Zone"), ]
-
-# Spatial files
-state.mp <- readOGR("data/spatial/test1.shp")
-# wgs.84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-
-commonwealth.mp <- readOGR("data/spatial/AustraliaNetworkMarineParks.shp")
-
-# commonwealth.mp$ZoneIUCN <- str_replace_all(commonwealth.mp$ZoneIUCN,c("IA"="Ia"))
-
-commonwealth.mp$ZoneIUCN <- fct_collapse(commonwealth.mp$ZoneIUCN,
-                                         Ia = c("Ia", "IA"),
-                                         II = "II",
-                                         IV = "IV",
-                                         VI = "VI")
-
-commonwealth.mp$IUCN <- as.factor(commonwealth.mp$ZoneIUCN)
-
-unique(commonwealth.mp$IUCN)
-factpal <- colorFactor(c("#f6c1d9", "#7bbc63",
-                         "#fff7a3","#6daee0"), commonwealth.mp$IUCN)
-
-testpal<-colorFactor(c("#f6c1d9", "#7bbc63",
-                       "#fff7a3","#6daee0"),
-                     levels=c("Ia", "II", "IV", "VI"))
-
-#f6c1d9 - pink 1A, 1a
-#7bbc63 - green IUCN II
-#fff7a3 - yellow IUCN IV
-#b9e6fb - light blue VI
-#6daee0 - dark blue VI
--- server.R
+++ server.R
@@ -54,19 +54,29 @@ function(input, output, session) {
                         popupOptions=c(closeButton = TRUE,
                                        minWidth = 0,maxWidth = 500))%>%
       
+      # Ngari Capes Marine Parks
+      addPolygons(data = ngari.mp, weight = 1, color = "black", 
+                  fillOpacity = 0.5, fillColor = "#c1d72f", 
+                  group = "State marine parks", label=ngari.mp$Name)%>%
+      
       # State Marine Parks
       addPolygons(data = state.mp, weight = 1, color = "black", 
-                  fillOpacity = 0.5, fillColor = "#7bbc63", 
-                  group = "State marine parks", label=state.mp$Name)%>%
+                  fillOpacity = 0.5, fillColor = ~state.pal(zone), 
+                  group = "State marine parks", label=state.mp$COMMENTS)%>%
+      
+      # Add a legend
+      addLegend(pal = state.pal, values = state.mp$zone, opacity = 1,
+                title="State Zones",
+                position = "bottomright", group = "State marine parks")%>%
       
       # Commonwealth Marine Parks
       addPolygons(data = commonwealth.mp, weight = 1, color = "black", 
-                  fillOpacity = 0.5, fillColor = ~factpal(IUCN), 
+                  fillOpacity = 0.5, fillColor = ~commonwealth.pal(zone), 
                   group = "Commonwealth marine parks", label=commonwealth.mp$ZoneName)%>%
       
       # Add a legend
-      addLegend(pal = testpal, values = commonwealth.mp$IUCN, opacity = 1,
-                title="IUCN Protected Area Category",
+      addLegend(pal = commonwealth.pal, values = commonwealth.mp$zone, opacity = 1,
+                title="Commonwealth Zones",
                 position = "bottomright", group = "Commonwealth marine parks")%>%
       # 
       # # Add a legend
